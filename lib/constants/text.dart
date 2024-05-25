7951cc698217e79b442ced3101132f74da43acd9
arnav062008 <arnavpalawat@gmail.com>
2024-05-25 10:11:27 -0400

    Commit constants and landing

--- lib/constants/text.dart
+++ lib/constants/text.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 const announcementTextStyle = TextStyle();
 
-const headerTextColors = [
-  Color(0xFFFFFFFF);
-
-]
+const headerTextColors = [Color(0xFFFFFFFF), Color(0xFF3C5FA4)];
