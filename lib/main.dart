0c73dc9b4ee42e645794c3aaae870867aa1c3f71
arnav062008 <arnavpalawat@gmail.com>
2024-05-25 21:49:35 -0400

    Firebase commit basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:internhs/screens/landing_page.dart';
 
-void main() {
-  Paint.enableDithering = true;
-  runApp(const MyApp());
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const InternHS());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class InternHS extends StatelessWidget {
+  const InternHS({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+    return const MaterialApp(
+      title: 'InternHS',
       home: LandingPage(),
     );
   }
